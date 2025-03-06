c417e473ea8f71d902111c8907a00b8cd4e4726c
harshps900 <151460810+harshps900@users.noreply.github.com>
2025-03-06 23:54:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,16 +12,16 @@ function delayedPromise(data, delay) {
 }
 
 // Start the promise chain
-delayedPromise(numbers, 1000)  // Delay changed to 1000ms
+delayedPromise(numbers, 2000)  // Increase delay to 2000ms
     .then((data) => {
         // First transformation: Filter even numbers
         const evenNumbers = data.filter(num => num % 2 === 0);
-        return delayedPromise(evenNumbers, 1000);  // Delay remains 1000ms
+        return delayedPromise(evenNumbers, 2000);  // Increase delay to 2000ms
     })
     .then((evenNumbers) => {
         // Show filtered even numbers
         output.textContent = evenNumbers.join(", ");
-        return delayedPromise(evenNumbers.map(num => num * 2), 2000);  // Delay remains 2000ms
+        return delayedPromise(evenNumbers.map(num => num * 2), 2000);  // Keep delay as 2000ms
     })
     .then((doubledNumbers) => {
         // Show even numbers multiplied by 2
